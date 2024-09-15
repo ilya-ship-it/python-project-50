fa842c22f2d432518d58ccec6e5f4eae988a4fcd
ilya-ship-it <tulupov.2712@gmail.com>
2024-09-15 05:35:59 +0300

    Update stylish

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -5,25 +5,25 @@ def stylish(diff):
 
 def tree_view(diff, depth=1):
     lines = []
-    for key, (type, value) in sorted(diff.items()):
-        lines = create_formatted_line(lines, key, value, depth, type)
+    for key, (action, value) in sorted(diff.items()):
+        lines = create_formatted_line(lines, key, value, depth, action)
     result = '\n'.join(lines)
     return result
 
 
-def create_formatted_line(lines, key, value, depth, type):
+def create_formatted_line(lines, key, value, depth, action):
     prefix = '  '
-    if type == 'nested':
+    if action == 'nested':
         indent = (4 * depth - 2) * ' '
         child_diff = tree_view(value, depth + 1)
         formated_value = f'{{\n{child_diff}\n{indent}  }}'
         lines = add_indent_and_format(depth, lines, prefix, key, formated_value)
-    elif type == 'changed':
+    elif action == 'changed':
         lines = changed_data_diff(lines, value, depth, key)
-    elif type == 'added':
+    elif action == 'added':
         prefix = '+ '
         lines = add_indent_and_format(depth, lines, prefix, key, value)
-    elif type == 'removed':
+    elif action == 'removed':
         prefix = '- '
         lines = add_indent_and_format(depth, lines, prefix, key, value)
     else:
