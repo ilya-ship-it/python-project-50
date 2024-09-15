708afcc604e202221775842ddd129ce841c2ca1c
ilya-ship-it <tulupov.2712@gmail.com>
2024-09-15 04:28:13 +0300

    Update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,9 +4,13 @@ from gendiff.scripts.gendiff import generate_diff
 
 @pytest.mark.parametrize("file_1, file_2, expected", [
     ("tests/fixtures/filepath1.json", "tests/fixtures/filepath2.json",
-    "tests/fixtures/expected_json_1.txt"),
+    "tests/fixtures/expected_1.txt"),
     ("tests/fixtures/filepath1.yaml", "tests/fixtures/filepath2.yaml",
-    "tests/fixtures/expected_yaml_1.txt")
+    "tests/fixtures/expected_1.txt"),
+    ("tests/fixtures/filepath3.json", "tests/fixtures/filepath4.json",
+    "tests/fixtures/expected_2.txt"),
+    ("tests/fixtures/filepath3.yaml", "tests/fixtures/filepath4.yaml",
+    "tests/fixtures/expected_2.txt")
     ])
 def test_generate_diff(file_1, file_2, expected):
     diff = generate_diff(file_1, file_2)
