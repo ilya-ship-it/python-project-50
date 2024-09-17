3e85070e2b3bf11264f446956c711e6de8a45dba
ilya-ship-it <tulupov.2712@gmail.com>
2024-09-17 20:14:58 +0300

    Lint fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -14,23 +14,27 @@ formaters = {
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.')
+        description='Compares two configuration files and shows a difference.'
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output',
-                        default='stylish')
+                        default='stylish'
+                        )
     args = parser.parse_args()
+
     file_path_1 = args.first_file
     file_path_2 = args.second_file
     formater = args.format
+
     diff = generate_diff(file_path_1, file_path_2, formater)
     print(diff)
 
 
 def generate_diff(file_path_1, file_path_2, format_name='stylish'):
-    fisrt_file = parse(file_path_1)
+    first_file = parse(file_path_1)
     second_file = parse(file_path_2)
-    diff = get_diff(fisrt_file, second_file)
+    diff = get_diff(first_file, second_file)
     diff = formaters[format_name](diff)
     return diff
 
