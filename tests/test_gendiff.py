4e8ed126042e6ae68b2b79b6f82924ad8f5a9eca
ilya-ship-it <tulupov.2712@gmail.com>
2024-09-17 20:17:57 +0300

    Lint fix

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,22 +4,23 @@ from gendiff.scripts.gendiff import generate_diff
 
 @pytest.mark.parametrize("file_1, file_2, expected, formater", [
     ("tests/fixtures/file1.json", "tests/fixtures/file2.json",
-    "tests/fixtures/expected_1.txt", "stylish"),
+     "tests/fixtures/expected_1.txt", "stylish"),
     ("tests/fixtures/filepath1.yaml", "tests/fixtures/filepath2.yaml",
-    "tests/fixtures/expected_1.txt", "stylish"),
+     "tests/fixtures/expected_1.txt", "stylish"),
     ("tests/fixtures/file3.json", "tests/fixtures/file4.json",
-    "tests/fixtures/expected_2.txt", "stylish"),
+     "tests/fixtures/expected_2.txt", "stylish"),
     ("tests/fixtures/file1.yml", "tests/fixtures/file2.yml",
-    "tests/fixtures/expected_2.txt", "stylish"),
+     "tests/fixtures/expected_2.txt", "stylish"),
     ("tests/fixtures/file3.json", "tests/fixtures/file4.json",
-    "tests/fixtures/expected_3.txt", "plain"),
+     "tests/fixtures/expected_3.txt", "plain"),
     ("tests/fixtures/file3.json", "tests/fixtures/file4.json",
-    "tests/fixtures/expected_4.txt", "json")   
-    ])
+     "tests/fixtures/expected_4.txt", "json")
+])
 def test_generate_diff(file_1, file_2, expected, formater):
     diff = generate_diff(file_1, file_2, formater)
     expected_result = read_file(expected)
     assert diff == expected_result
+
 
 def read_file(file_name):
     with open(file_name) as file:
