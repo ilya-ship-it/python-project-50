22f0f8256ecbdc9c22f965304854b1fad08e7460
ilya-ship-it <tulupov.2712@gmail.com>
2024-09-17 19:45:59 +0300

    Fix gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -17,7 +17,8 @@ def main():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output',
+                        default='stylish')
     args = parser.parse_args()
     file_path_1 = args.first_file
     file_path_2 = args.second_file
