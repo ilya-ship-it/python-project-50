ab46be64ec85d9dfefce35dba764dc8eab6488ae
ilya-ship-it <tulupov.2712@gmail.com>
2024-09-18 21:43:36 +0300

    Refactor gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,6 @@
 import argparse
-from gendiff.scripts.parser import parse
-from gendiff.scripts.get_diff import get_diff
+from gendiff.parser import parse
+from gendiff.get_diff import get_diff
 from gendiff.formaters.stylish import stylish
 from gendiff.formaters.plain import plain
 from gendiff.formaters.json import json_format
@@ -32,11 +32,19 @@ def main():
 
 
 def generate_diff(file_path_1, file_path_2, format_name='stylish'):
-    first_file = parse(file_path_1)
-    second_file = parse(file_path_2)
+    data_1, data_format_1 = read_data(file_path_1)
+    data_2, data_format_2 = read_data(file_path_2)
+    first_file = parse(data_1, data_format_1)
+    second_file = parse(data_2, data_format_2)
     diff = get_diff(first_file, second_file)
     diff = formaters[format_name](diff)
     return diff
+
+
+def read_data(file_path):
+    file_format = file_path.split('.')[-1]
+    with open(file_path) as file:
+        return file.read(), file_format
 
 
 if __name__ == "__main__":
